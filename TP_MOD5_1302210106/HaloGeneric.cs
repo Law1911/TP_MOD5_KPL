83da9e0525fe9a07c0b9ed9a7506bb6f3a919187
Law1911 <epensusanto@gmail.com>
2023-03-19 21:23:42 +0700

    menambahkan class HaloGeneric dengan method

---
+++ TP_MOD5_1302210106/HaloGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MOD5_1302210106
+{
+    internal class HaloGeneric<T>
+    {
+        public void HaloUSer(T nama)
+        {
+            Console.WriteLine("Hallo User, " + nama);
+        }
+    }
+}
--- TP_MOD5_1302210106/Program.cs
+++ TP_MOD5_1302210106/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace TP_MOD5_1302210106
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric<String> halo = new HaloGeneric<String>();
+
+            String X = "Stevent Rangga Ramaditya";
+
+            halo.HaloUSer(X);
+        }
+    }
+}
